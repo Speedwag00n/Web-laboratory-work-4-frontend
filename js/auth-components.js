e9a42714f273b48cc412fbdea659880905dd6b18
Ilia Nemankov <ilia.nemankov@gmail.com>
2019-12-07 00:19:38 +0200

    Add field validation messages

--- js/auth-components.js
+++ js/auth-components.js
@@ -1,3 +1,22 @@
+var authValidationMixin = {
+    computed: {
+        validLoginLenght: function() {
+            if (this.login.length < 6) {
+                return false;
+            } else {
+                return true;
+            }
+        },
+        validPasswordLength: function() {
+            if (this.password.length < 6) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+}
+
 Vue.component(
     'login-form',
     {
@@ -5,9 +24,11 @@ Vue.component(
             `
             <div class="horizontal-centering-container">
                 <label class="default-label">Логин</label>
-                <input class="input-param" type="text" v-model="login"/>
+                <input class="input-param" type="text" v-model.lazy="login"/>
+                <span v-show="login && !validLoginLenght" class="default-warning">Длина логина должна быть не менее 6 символов</span>
                 <label class="default-label">Пароль</label>
-                <input class="input-param" type="password" v-model="password"/>
+                <input class="input-param" type="password" v-model.lazy="password"/>
+                <span v-show="password && !validPasswordLength" class="default-warning">Длина пароля должна быть не менее 6 символов</span>
                 <div>
                     <button class="submit-button">Авторизоваться</button>
                 </div>
@@ -25,7 +46,8 @@ Vue.component(
             onTabChange: function() {
                 this.$emit('tabchange', false);
             }
-        }
+        },
+        mixins: [authValidationMixin]
     }
 );
 
@@ -36,11 +58,14 @@ Vue.component(
             `
             <div class="horizontal-centering-container">
                 <label class="default-label">Логин</label>
-                <input class="input-param" type="text" v-model="login"/>
+                <input class="input-param" type="text" v-model.lazy="login"/>
+                <span v-show="login && !validLoginLenght" class="default-warning">Длина логина должна быть не менее 6 символов</span>
                 <label class="default-label">Пароль</label>
-                <input class="input-param" type="password" v-model="password"/>
+                <input class="input-param" type="password" v-model.lazy="password"/>
+                <span v-show="password && !validPasswordLength" class="default-warning">Длина пароля должна быть не менее 6 символов</span>
                 <label class="default-label">Подтвердите пароль</label>
-                <input class="input-param" type="password" v-model="confirmedPassword"/>
+                <input class="input-param" type="password" v-model.lazy="confirmedPassword"/>
+                <span v-show="confirmedPassword && !passwordsMatch" class="default-warning">Пароли должны совпадать</span>
                 <div>
                     <button class="submit-button">Зарегистрироваться</button>
                 </div>
@@ -58,6 +83,12 @@ Vue.component(
         methods: {
             onTabChange: function() {
                 this.$emit('tabchange', true);
+            }
+        },
+        mixins: [authValidationMixin],
+        computed: {
+            passwordsMatch: function() {
+                return this.password === this.confirmedPassword
             }
         }
     }
